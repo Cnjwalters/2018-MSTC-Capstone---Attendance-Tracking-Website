bb9831d2cd9ee31e4752c0661f35600d8c3c100e
Cnjwalters <33673430+Cnjwalters@users.noreply.github.com>
2018-12-18 09:12:53 -0600

    Add files via upload
    Adding SQL file, containing the database structure.

---
+++ Documentation/ERD/attendancetracking.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 18, 2018 at 09:07 AM
+-- Server version: 5.7.16-log
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `attendancetracking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courseinfo`
+--
+
+CREATE TABLE `courseinfo` (
+  `courseID` int(8) NOT NULL,
+  `employeeID` int(8) NOT NULL,
+  `courseName` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `encforgottenpassword`
+--
+
+CREATE TABLE `encforgottenpassword` (
+  `encryption` varchar(128) NOT NULL,
+  `username` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `encuserconfirm`
+--
+
+CREATE TABLE `encuserconfirm` (
+  `encryption` varchar(128) NOT NULL,
+  `username` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollment`
+--
+
+CREATE TABLE `enrollment` (
+  `courseID` int(11) NOT NULL,
+  `studentID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentinfo`
+--
+
+CREATE TABLE `studentinfo` (
+  `studentID` int(8) NOT NULL,
+  `studentFirstName` varchar(50) NOT NULL,
+  `studentLastName` varchar(62) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `employeeId` int(8) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `confirmation` int(1) NOT NULL,
+  `email` varchar(62) NOT NULL,
+  `securityQuestion` varchar(120) DEFAULT NULL,
+  `securityQuestionAnswer` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courseinfo`
+--
+ALTER TABLE `courseinfo`
+  ADD PRIMARY KEY (`courseID`),
+  ADD KEY `courseID` (`courseID`);
+
+--
+-- Indexes for table `enrollment`
+--
+ALTER TABLE `enrollment`
+  ADD KEY `courseID` (`courseID`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `courseID_2` (`courseID`,`studentID`);
+
+--
+-- Indexes for table `studentinfo`
+--
+ALTER TABLE `studentinfo`
+  ADD PRIMARY KEY (`studentID`),
+  ADD KEY `studentID` (`studentID`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`employeeId`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enrollment`
+--
+ALTER TABLE `enrollment`
+  ADD CONSTRAINT `enrollment_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `courseinfo` (`courseID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `enrollment_ibfk_2` FOREIGN KEY (`studentID`) REFERENCES `studentinfo` (`studentID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
